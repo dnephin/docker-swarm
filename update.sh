5f607d008105a6ce593a4ed02a2674e30d0d8421
Daniel Nephin <dnephin@gmail.com>
2015-09-24 18:23:58 -0400

    Restore original update script and a statically linked binary.

--- update.sh
+++ update.sh
@@ -1,5 +1,33 @@
-#!/bin/sh
+#!/bin/bash
+
 set -e
-mkdir -p ./dev
-GOPATH=`pwd`/dev go get github.com/docker/swarm
-cp ./dev/bin/swarm ./swarm
+
+if [ $# -eq 0 ] ; then
+	echo "Usage: ./update.sh <docker/swarm tag or branch>"
+	exit
+fi
+
+VERSION=$1
+
+# cd to the current directory so the script can be run from anywhere.
+cd `dirname $0`
+
+echo "Fetching and building swarm $VERSION..."
+
+# Create a temporary directory.
+TEMP=`mktemp -d`
+
+git clone -b $VERSION https://github.com/docker/swarm.git $TEMP
+docker build -t swarm-builder $TEMP
+
+# Create a dummy swarmbuild container so we can run a cp against it.
+ID=$(docker create swarm-builder)
+
+# Update the local binary.
+docker cp $ID:/go/bin/swarm .
+
+# Cleanup.
+docker rm -f $ID
+docker rmi swarm-builder
+
+echo "Done."
